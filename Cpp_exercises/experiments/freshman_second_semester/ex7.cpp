39a34af33cd7e636f862194e7a9e75736c8d278d
Egrvigrf <2455737667@qq.com>
2024-05-13 12:23:47 +0800

    add cf div4 round944

--- Cpp_exercises/experiments/freshman_second_semester/ex7.cpp
+++ Cpp_exercises/experiments/freshman_second_semester/ex7.cpp
@@ -1,50 +1,40 @@
 #include <iostream>
-// 基类
+using namespace std;
 class Vehicle
 {
 public:
     void move()
     {
-        std::cout << "Vehicle moves." << std::endl;
+        cout << "Vehicle moves." << endl;
     }
 };
-
-// 第一个派生类
 class Car : virtual public Vehicle
 {
 public:
-    // 重写基类的move函数
     void move()
     {
-        std::cout << "Car moves." << std::endl;
+        cout << "Car moves." << endl;
     }
 };
-
-// 第二个派生类
 class Airplane : virtual public Vehicle
 {
 public:
-    // 重写基类的move函数
     void move()
     {
-        std::cout << "Airplane moves." << std::endl;
+        cout << "Airplane moves." << endl;
     }
 };
-
-// 最终类
 class FlyingCar : public Car, public Airplane
 {
 public:
-    // FlyingCar类继承了两个基类，但由于使用了虚拟继承，这里不会产生二义性
     void move()
     {
-        std::cout << "FlyingCar moves." << std::endl;
+        cout << "FlyingCar moves." << endl;
     }
 };
-
 int main()
 {
     FlyingCar fc;
-    fc.move(); // FlyingCar类中的move函数不会产生二义性
+    fc.move();
     return 0;
 }
